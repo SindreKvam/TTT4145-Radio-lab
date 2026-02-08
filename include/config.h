c92e6e25d37e4a81ee32592e6bf555e0ac027f54
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2026-02-08 15:51:11 +0100

    Updated defined macros for configurations

--- include/config.h
+++ include/config.h
@@ -3,6 +3,12 @@
 #define MHZ(x) ((long long)((x)*1000000.0))
 #define GHZ(x) ((long long)((x)*1000000000.0))
 
+// Root raised cosine parameters
+#define RRC_BETA 0.5f
+#define RRC_SPAN 10
+#define RRC_SPS 4 /** Sample per symbol */
+
+/* Rx */
 #define RX_SAMPLING_RATE_DEFAULT MHZ(5.0)
 #define RX_RF_BANDWIDTH_DEFAULT MHZ(10)
 #define RX_LO_FREQUENCY_DEFAULT GHZ(2.4)
@@ -12,11 +18,7 @@
 
 #define RX_QUEUE_SIZE 3
 
-#define I_Q_CHANNEL_BUFFER_SIZE (RX_BUFFER_SIZE / 2)
-
-// Root raised cosine parameters
-#define RRC_BETA 0.5f
-#define RRC_SPAN 10
-#define RRC_SPS 4 /** Sample per symbol */
-
-// Symbol rate (data rate * M (for M-QAM)) = SAMPLING_RATE / SPS
+/* Tx */
+#define TX_SYMBOL_RATE_DEFAULT (RX_SAMPLING_RATE_DEFAULT / 4)
+#define TX_RF_BANDWIDTH_DEFAULT MHZ(10)
+#define TX_LO_FREQUENCY_DEFAULT GHZ(2.4)
