83cb059910a6ad697dd99946d27890d7be1c3a89
Even Petersen <Petersen.Even@gmail.com>
2026-02-09 17:34:43 +0100

    have made a clean vorking code using classes and eliminated the need for csv

--- src/tx/modulators.cpp
+++ src/tx/modulators.cpp
@@ -1,115 +1,112 @@
 #include <iostream>
-#include <random>
 #include <vector>
-#include <ctime>
 #include <cmath>
-#include "fault_protection.h"
 #include <complex>
-#include <fstream>
+#include <algorithm>
 
-/*Making LUT for M-QAM refferencable by int*/
-std::vector<std::vector<int>>create_LUT(){
-    std::fstream csv("gray_codes/graycodes.csv", std::ios::in);
 
-    std::vector<std::vector<std::string>>LUT_string(0, std::vector<std::string>(3));
-    std::vector<std::vector<int>>LUT(0, std::vector<int>(3));
+class Modulator{
+    private:
 
-    std::string line;
-    std::string num;
-    int iterator = 0;
-    int iterator_2 = 0;
+        std::vector<std::vector<int>>LUT;
 
-    while (std::getline(csv, line)) {
-        std::stringstream ss(line);
-        std::vector<std::string> row(3);
+        std::vector<std::vector<int>>generate_LUT(int size){//generates Look Up Table for M-QAM
+            std::vector<std::vector<int>>graycodes(size, std::vector<int>(2));
+            //makes a 2d vector
+            std::vector<std::vector<int>>graycode_grid(static_cast<int>(std::sqrt(size)),std::vector<int>(static_cast<int>(std::sqrt(size)))); 
+            std::vector<int> horisontal_gray_codes(std::sqrt(size));
+            std::vector<int> vertical_gray_codes(std::sqrt(size));
 
-        for (int i = 0; i < 3 && std::getline(ss, num, ','); ++i) {
-            row[i] = num;
+            //creates horisontal gray codes
+            for (int i = 1; i < std::sqrt(size); i++){
+                horisontal_gray_codes[i] = i ^ (i >> 1); 
+            }
+
+            vertical_gray_codes = horisontal_gray_codes;
+            //shiftoperation to all elements in array to the left
+            std::transform(vertical_gray_codes.begin(), vertical_gray_codes.end(), vertical_gray_codes.begin(),
+            [size](int x){return x << static_cast<int>(std::log2(size)) / 2;});
+
+            for (int i = 0; i < vertical_gray_codes.size(); i++){
+                for (int j = 0; j < horisontal_gray_codes.size(); j++){
+                    graycode_grid[i][j] = vertical_gray_codes[i] ^ horisontal_gray_codes[j];// just for visualization
+                    graycodes[graycode_grid[i][j]] = {i, j};
+                }
+            }
+            //only for visualisation
+            // for (int i = 0; i < vertical_gray_codes.size(); i++){
+            //     for (int j = 0; j < horisontal_gray_codes.size(); j++){
+            //         std::cout<<graycode_grid[i][j]<<" ";
+            //     }
+            //     std::cout<<std::endl;
+            // }
+
+            return graycodes;
+        }
+        std::complex<double> PSK_modulation(uint16_t number_to_modulate){//do PSK modulation based on current settings give complex modulation
+            std::vector<double> I_and_Q(2);
+            uint16_t gray_encoding = number_to_modulate ^ (number_to_modulate >> 1);
+            double theta = 2 * M_PI * gray_encoding / num_of_symbols+ M_PI / num_of_symbols;
+
+            std::complex<double> IQ_complex = std::polar(1.,theta);
+            return IQ_complex;
+        }
+        std::complex<double>QAM_modulation(uint16_t number_to_modulate){//do QAM modulation based on current settings give complex modulation
+            std::complex<double> modulation;
+            std::vector<int>place_in_graycode_grid = LUT[number_to_modulate];
+
+
+            int shift = (std::sqrt(num_of_symbols)-1);
+            place_in_graycode_grid[0] = place_in_graycode_grid[0]*2 - shift;//some sick mappping getting centre at 0 and peaks at sqrt(M)-1
+            place_in_graycode_grid[1] = place_in_graycode_grid[1]*2 - shift;
+
+            double normalized_real = place_in_graycode_grid[1]/(std::sqrt(num_of_symbols) - 1);
+            double normalized_imag = -place_in_graycode_grid[0]/(std::sqrt(num_of_symbols) - 1);//minus just to correspond with sketch
+
+            modulation.real(normalized_real);
+            modulation.imag(normalized_imag);
+
+            //std::cout<<modulation.real()<<" "<<modulation.imag()<<std::endl;
+
+            return modulation;
         }
 
-        LUT_string.push_back(row);
-    }
-
-    for (std::vector<std::string> symbol: LUT_string){
-        std::vector<int> numeric_line(3);
-
-        //std::cout<<symbol[0]<<' '<<symbol[1]<<' '<<symbol[2]<<std::endl;
-        if (std::isdigit(symbol[0][0])){
-            numeric_line = {std::stoi(symbol[0]), std::stoi(symbol[1]), std::stoi(symbol[2])};
-            LUT.push_back(numeric_line);
+    public:
+        int num_of_symbols;
+        std::string mod_scheme;
+        
+        //instantiate the modulator
+        Modulator(std::string mod_scheme = "QAM", int num_of_symbols = 16){
+            this->mod_scheme = mod_scheme;
+            this->num_of_symbols = num_of_symbols;
+            LUT = generate_LUT(num_of_symbols);
         }
-    }
 
-    // for (std::vector<int> symbol: LUT){
-    //     std::cout<<symbol[0]<<' '<<symbol[1]<<' '<<symbol[2]<<std::endl;
-    // }
-    return LUT;
-}
-    
-
-
-/*this gives the modulation of Phase Shift keying with M symbols and return a vector with 
-the corresponding real and complex element, both as real values*/
-std::vector<double> MPSK(uint16_t symbol, uint16_t number_of_symbols){
-    std::vector<double> I_and_Q(2);
-    uint16_t gray_encoding = symbol ^ (symbol >> 1);
-    double theta = 2 * M_PI * gray_encoding / number_of_symbols+ M_PI / number_of_symbols;
-
-    I_and_Q[0] = cos(theta);   // I
-    I_and_Q[1] = sin(theta);   // Q
-
-    return I_and_Q;
-}
-
-
-
-/*this gives the modulation of Phase Shift keying with M symbols and return a complex number
- with the correct phase and amplitude */
-std::complex<double> MPSK_complex(uint16_t symbol, uint16_t number_of_symbols){
-    std::vector<double> I_and_Q(2);
-    uint16_t gray_encoding = symbol ^ (symbol >> 1);
-    double theta = 2 * M_PI * gray_encoding / number_of_symbols+ M_PI / number_of_symbols;
-
-    std::complex<double> IQ_complex = std::polar(1.,theta);
-    return IQ_complex;
-}
-
-
-
-/*M-QAM returning the symbol as complex*/
-std::complex<double> M_QAM_complex(uint16_t symbol, uint16_t number_of_symbols = 16){
-    std::complex<double> modulation;
-    std::vector<int> looked_up(3);
-    static std::vector<std::vector<int>>LUT = create_LUT();
-
-    //using the LUT to find the correct constillation
-    for (std::vector<int>constilation:LUT){//constilation = [symbol, x_pos, y_pos]
-        if (constilation[0]==symbol){
-            looked_up = constilation;
-            break;
+        //modulate a number based on the current modulation settings
+        std::complex<double>modulate(uint16_t number_to_modulate){
+            std::complex<double>modulation;
+            if (mod_scheme == "QAM"){
+                modulation = QAM_modulation(number_to_modulate);
+            }
+            else if(mod_scheme == "PSK"){
+                modulation = PSK_modulation(number_to_modulate);
+            }
+            return modulation;
         }
-    }
 
-    int shift = (std::sqrt(number_of_symbols)-1);
-    looked_up[1] = looked_up[1]*2 - shift;//some sick mappping getting centre at 0 and peaks at sqrt(M)-1
-    looked_up[2] = looked_up[2]*2 - shift;
-
-    double normalized_real = looked_up[2]/(std::sqrt(number_of_symbols) - 1);
-    double normalized_imag = -looked_up[1]/(std::sqrt(number_of_symbols) - 1);//minus just to correspond with sketch
-
-    modulation.real(normalized_real);
-    modulation.imag(normalized_imag);
-
-    //std::cout<<modulation.real()<<" "<<modulation.imag()<<std::endl;
-
-    return modulation;
-}
-
+        //prints the current look up table in order for debugging
+        void print_LUT(){
+            for (int i = 0; i < LUT.size(); i++){
+                std::cout<<"index "<<i<<" in LUT is "<<LUT[i][0]<<" "<<LUT[i][1]<<std::endl;
+            }
+        }
+};
 
 
 // int main(){
-//     //std::cout<<MPSK_complex(0101, 16)<<std::endl;
-//     //create_LUT();
-//     //QPSK_complex(16);
-//     std::cout<<M_QAM_complex(13)<<std::endl;
+//     Modulator QAM_16("QAM",64);
+
+//     for (int i = 0; i < QAM_16.num_of_symbols; i++){
+//         std::cout<<i<<" gets modulated to "<<QAM_16.modulate(i)<<std::endl;
+//     }
 // }
