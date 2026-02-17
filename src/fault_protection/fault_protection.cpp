1a3e9ae6529088e411ce381a0fe9da56b6c79e2d
Even Petersen <Petersen.Even@gmail.com>
2026-02-17 16:46:03 +0100

    something horrible with fault protector and fried brain

--- src/fault_protection/fault_protection.cpp
+++ src/fault_protection/fault_protection.cpp
@@ -3,14 +3,24 @@
 #include <vector>
 #include <ctime>
 #include <cmath>
+#include <algorithm>
 
 #include "fault_protection.h"
 
 void Hamming::print_internals(){
     //printing the buffer
-    std::cout<<"buffer = ";
-    for (auto i : this->buffer){
-        std::cout<<i<<" ";
+    int index = 0;
+    std::cout<<"buffer = "<<std::endl;
+    for (auto i : this->tx_buffer){
+        uint16_t bitmask = 1<<15;
+        while(bitmask){
+            if (bitmask & i){std::cout<<1;}
+            else{std::cout<<0;}
+            bitmask >>= 1;
+        }
+        std::cout << " = " << i << " index = " << index;
+        std::cout<<std::endl;
+        index++;
     }
     std::cout<<std::endl;
 
@@ -22,10 +32,12 @@ void Hamming::print_internals(){
     std::cout<<std::endl;
 }
 
-Hamming::Hamming(uint16_t window_size){
+Hamming::Hamming(int window_size){
     int parity_bits_amount = int(std::log2(window_size))+1;
-    this->tx_buffer.reserve(window_size);
+    tx_buffer.resize(window_size,0);// makes the vector fill with zeros to the correct size
+
     this->index_of_parity_bits.reserve(parity_bits_amount);
+    this->window_size = window_size;
 
     //get to know where to put the error correction bits
     index_of_parity_bits.push_back(0);
@@ -37,13 +49,77 @@ Hamming::Hamming(uint16_t window_size){
 }
 
 void Hamming::put_into_buffer(uint16_t value){
-    uint16_t bitmask = 0b1000000000000000;
-    while (bitmask > 0){
+    //are we on a bit index dedicated to parity bits?
+    while(std::find(index_of_parity_bits.begin(), index_of_parity_bits.end(), index_tx_buffer) != index_of_parity_bits.end()){
+        index_tx_buffer++;
 
+    }
+    tx_buffer[index_tx_buffer] = value;   
+    
+    index_tx_buffer++;
+    //setting flag
+    if (index_tx_buffer == window_size){
+        ready_to_send_buffer = true;
     }
 }
 
+// this sucks.... not functional !!!!
+void Hamming::encode(){
+    uint16_t bitmask = 1;
+    std::vector<uint16_t> flipper(window_size, 0); // will be used to figure what bits to flip
+    
+    int h_index = 0; //horizontal index
+    while (bitmask){
+        for (int i = 0; i < window_size; i++){
+            if (bitmask & tx_buffer[i]){
+                flipper[h_index] ^= i;    
+            }
+        }
+        bitmask>>=1;
+        h_index++;
+    }
+
+    bitmask = 1 << (int(std::log2(window_size))-1);
+    while (bitmask){
+        for (int i = 0; i < window_size; i++){
+            if(bitmask & flipper[i]){
+                tx_buffer[bitmask] |= bitmask;
+            }
+        }
+        bitmask >>= 1;
+    }
+}
+
+void Hamming::force_encode(){
+    encode();
+}
+
+void Hamming::decode(){
+
+}
+
 // int main(){
+//     int size = 16;
+//     int i = 0;
 //     Hamming ham = Hamming(16);
+//     // while (!ham.ready_to_send_buffer){
+//     //     ham.put_into_buffer(i);
+//     //     i++;
+//     // }
+//     ham.put_into_buffer(1);
+//     ham.put_into_buffer(0);
+//     ham.put_into_buffer(1);
+//     ham.put_into_buffer(0);
+//     ham.put_into_buffer(1);
+//     ham.put_into_buffer(0);
+//     ham.put_into_buffer(1);
+//     ham.put_into_buffer(0);
+//     ham.put_into_buffer(1);
+//     ham.put_into_buffer(0);
+//     ham.put_into_buffer(1);
+
+//     ham.force_encode();
+    
+    
 //     ham.print_internals();
 // }
--- src/fault_protection/fault_protection.h
+++ src/fault_protection/fault_protection.h
@@ -1,29 +1,32 @@
 #pragma once
 
-#include <vector>
-#include <cstdint>
 #include <iostream>
+#include <random>
+#include <vector>
+#include <ctime>
 #include <cmath>
+#include <algorithm>
 
 class Hamming{
     private:
-    uint16_t window_size; //will use log2(windowsize)+1 for fault detection
-    std::vector<uint16_t> tx_buffer;//contains 16 window of bits including error correction
-    std::vector<uint16_t> index_of_parity_bits;
+        int window_size; //will use log2(windowsize)+1 for fault detection
+        std::vector<uint16_t> tx_buffer;//contains 16 window of bits including error correction
+        std::vector<uint16_t> index_of_parity_bits;
 
-    bool ready_to_send_buffer = 0;
-    int index_window_buffer = 0;
-    uint16_t window_buffer_bitmask; // used to track the possition on internal window buffer bits
+        int index_tx_buffer = 0;// start inserting data on index 3 as the first 3 are for faultprotection
+        uint16_t window_buffer_bitmask; // used to track the possition on internal window buffer bits
 
-    void encode();
-    void decode();
+        void encode();
+        void decode();
+
 
     public:
-    Hamming(uint16_t window_size);
-    void put_into_buffer(uint16_t value);
-    void print_internals();
+        bool ready_to_send_buffer = false;
+        Hamming(int window_size);
+        void put_into_buffer(uint16_t value);
+        void print_internals();
 
-    void force_encode();
-    void force_decode();
+        void force_encode();
+        void force_decode();
 };
 
