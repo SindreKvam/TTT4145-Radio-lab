3fd91909de95c460011edd9c575fa3d34eb64ba0
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2026-02-26 19:06:10 +0100

    Updated pll simulations

--- simulations/pll.py
+++ simulations/pll.py
@@ -15,11 +15,13 @@ class state:
 
 @dataclass
 class parameters:
-    num_symbols = 1200
-    oversampling_factor = 8
+    num_symbols = 2000
     loop_bandwidth = 300_000  # Needs to be larger than 120 kHz
+    rf_frequency = 2_400_000_000
     sampling_rate = 50_000_000
-    frequency_offset = 50_000
+    oversampling_factor = 8
+    symbol_rate = sampling_rate / oversampling_factor
+    frequency_offset = -500
     k_p = 0.0222
     k_i = 2.4e-4
 
@@ -42,33 +44,33 @@ if __name__ == "__main__":
 
     fig, ax = plt.subplots(1, 1)
     ax.semilogx(omega, loop_filter_db)
+    ax.set_title("Loop filter amplitude response")
     ax.grid()
 
     # Generate data to transmit
-    data = np.array([0, 1, 2, 3] * (parameters.num_symbols // 4))
+    data = np.array([0, 1, 3, 2] * (parameters.num_symbols // 4))
     data_symbols = QPSK(data)
 
-    # noise = (
-    #     (np.random.randn(len(data_symbols)) + 1j * np.random.randn(len(data_symbols)))
-    #     / np.sqrt(2)
-    #     * np.sqrt(0.10)
-    # )
-
+    # Add white noise error
+    noise = (
+        (np.random.randn(len(data_symbols)) + 1j * np.random.randn(len(data_symbols)))
+        / np.sqrt(2)
+        * np.sqrt(0.02)
+    )
     # data_symbols += noise
+
+    # Add phase error
     n = np.arange(len(data_symbols))
     data_symbols *= np.exp(
-        1j * 2 * np.pi * parameters.frequency_offset / parameters.sampling_rate * n
+        1j * 2 * np.pi * parameters.frequency_offset / parameters.symbol_rate * n
     )
 
-    # Plot generated symbols as received on the Rx side
-    fig, ax = plt.subplots(1, 1, tight_layout=True)
-    ax.set_box_aspect(1)
-    ax.scatter(data_symbols.real, data_symbols.imag)
-    ax.grid()
-
     e = np.zeros(len(data_symbols))
+    theta = np.zeros(len(data_symbols))
+    phase_locked_data = np.zeros(len(data_symbols), dtype=complex)
     for i, x in enumerate(data_symbols):
         x *= np.exp(-1j * state.theta)
+        phase_locked_data[i] = x
 
         closest_symbol = QPSK_demod(x)
 
@@ -76,12 +78,27 @@ if __name__ == "__main__":
 
         state.integrator = state.integrator + parameters.k_i * e[i]
         state.theta += state.integrator + parameters.k_p * e[i]
+        theta[i] = state.theta
+
+    # Plot generated symbols as received on the Rx side
+    fig, ax = plt.subplots(1, 1, tight_layout=True)
+    ax.plot(e, label="error")
+    ax2 = ax.twinx()
+    ax2.plot(theta, label="theta", color="C1")
+    ax.set_ylabel("Error (rad)")
+    ax2.set_ylabel("Theta (rad)")
+
+    ax.legend()
+    ax2.legend()
+    ax.grid()
 
     # Plot generated symbols as received on the Rx side
     fig, ax = plt.subplots(1, 1, tight_layout=True)
     ax.set_box_aspect(1)
-    ax.plot(e)
-    ax.set_ylabel("Error (rad)")
+    ax.scatter(data_symbols.real, data_symbols.imag, alpha=0.2)
+    ax.scatter(
+        phase_locked_data.real, phase_locked_data.imag, alpha=n / len(data_symbols)
+    )
     ax.grid()
 
     plt.show()
