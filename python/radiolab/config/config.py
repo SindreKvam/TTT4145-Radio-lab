f4f67e0d7bfd926ca418b1a4f0901a21cde475d5
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2026-03-21 18:34:12 +0100

    added more configuration flags

--- python/radiolab/config/config.py
+++ python/radiolab/config/config.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Any
 
+import numpy as np
+
 
 @dataclass
 class PhyConfig:
@@ -10,7 +12,7 @@ class PhyConfig:
     samples_per_symbol: int = 8  # sps
     pll_preamble_length: int = 600
     codeword_length: int = 64
-    codeword_corr_threshold: int = 1000
+    codeword_corr_threshold: int = 1000 // np.log2(modulation_order)
 
     rrc_beta: float = 0.2
     rrc_span: int = 10
@@ -20,12 +22,20 @@ class PhyConfig:
 class RadioConfig:
     """Configuration directly on hardware layer"""
 
-    sample_rate = 30_000_000
-    rx_lo_hz = 2_400_000_000
-    tx_lo_hz = 2_400_000_000
-    rx_buffer_size = 4096 * 8 * 2
+    rx_rf_bandwidth: int = 40_000_000
+    sample_rate: int = 20_000_000
+    rx_buffer_size: int = 10240 * 8 * 4
+    rx_lo_hz: int = 2_400_000_000
+    tx_lo_hz: int = 2_400_000_000
+    tx_cyclic_buffer: bool = False
+    tx_hardwaregain_chan0 = -30
+    gain_control_mode_chan0 = (
+        "manual"  # Valid configurations: ["manual", "slow_attack", "fast_attack"]
+    )
 
-    time_to_fill_buffer = rx_buffer_size / sample_rate
+    quadrature_tracking_en = False
+
+    time_to_fill_buffer: float = rx_buffer_size / sample_rate
 
 
 @dataclass
